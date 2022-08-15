ea3b8f0fd46a597b810b0cccdc7580e0e2ff8e19
jscao222 <jscao222@gmail.com>
2022-08-14 20:04:52 -0400

    version1

---
+++ main.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include "manipulating.h"
+
+int main(void)
+{
+	 manipulating();
+	return 0;
+}
---
+++ manipulating.c
@@ -0,0 +1,50 @@
+// MANIPULATING SOURCE
+#define _CRT_SECURE_NO_WARNINGS 
+#define BUFFER_SIZE 80
+#include "manipulating.h"
+
+// V1
+void manipulating(void) {
+    //    printf("*** Start of Concatenating Strings Demo ***\n");
+    //    char string1[BUFFER_SIZE];
+    //    char string2[BUFFER_SIZE];
+    //    do {
+    //        printf("Type the 1st string (q - to quit):\n");
+    //        fgets(string1, BUFFER_SIZE, stdin);
+    //        string1[strlen(string1) - 1] = '\0';
+    //        if ((strcmp(string1, "q") != 0)) {
+    //            printf("Type the 2nd string:\n");
+    //            fgets(string2, BUFFER_SIZE, stdin);
+    //            string2[strlen(string2) - 1] = '\0';
+    //            strcat(string1, string2);
+    //            printf("Concatenated string is \'%s\'\n", string1);
+    //        }
+    //    } while (strcmp(string1, "q") != 0);
+    //    printf("*** End of Concatenating Strings Demo ***\n\n");
+    //}
+
+        // V2
+    printf("*** Starting of Comparing Strings Demo ***\n");
+    char compare1[BUFFER_SIZE];
+    char compare2[BUFFER_SIZE];
+    int result;
+    do {
+        printf("Type the 1st string to compare (q - to quit):\n");
+        fgets(compare1, BUFFER_SIZE, stdin);
+        compare1[strlen(compare1) - 1] = '\0';
+        if (strcmp(compare1, "q") != 0) {
+            printf("Type the 2nd string to compare:\n");
+            fgets(compare2, BUFFER_SIZE, stdin);
+            compare2[strlen(compare2) - 1] = '\0';
+            result = strcmp(compare1, compare2); // the variable result is equal to string compare 1 and string compare 2
+            if (result < 0)
+                printf("\'%s\' string is less than \'%s\'\n", compare1, compare2); // compares string 1 and 2 to confirm string 1 is less than string 2 
+            else if (result == 0)
+                printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);  // compares string 1 and 2 to see if they are equal 
+            else
+                printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2); // compares the result of string 1 and string 2 to see if string 1 
+                                                                                      // is greater than string 2
+        }
+    } while (strcmp(compare1, "q") != 0);
+    printf("*** End of Comparing Strings Demo ***\n\n");
+}
